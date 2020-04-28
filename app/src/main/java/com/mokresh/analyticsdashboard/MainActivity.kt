4f82090a36f5973088bc536dd71d8e0ed936453c
mohammedmokresh <mokresh.mohammed@gmail.com>
2020-04-29 01:00:49 +0800

    create and inject the view model

--- app/src/main/java/com/mokresh/analyticsdashboard/MainActivity.kt
+++ app/src/main/java/com/mokresh/analyticsdashboard/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.mokresh.analyticsdashboard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.mokresh.analyticsdashboard.databinding.ActivityMainBinding
+import com.mokresh.analyticsdashboard.ui.AnalyticsViewModel
+import com.mokresh.analyticsdashboard.utils.SCOPES
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val analyticsViewModel:AnalyticsViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+
 
     }
 
--- app/src/main/java/com/mokresh/analyticsdashboard/ui/AnalyticsViewModel.kt
+++ app/src/main/java/com/mokresh/analyticsdashboard/ui/AnalyticsViewModel.kt
@@ -1,12 +1,51 @@
 package com.mokresh.analyticsdashboard.ui
 
+import android.view.View
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mokresh.analyticsdashboard.R
 import com.mokresh.analyticsdashboard.app.ApiServices
+import com.mokresh.analyticsdashboard.models.Analytics
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 
 class AnalyticsViewModel(
-    private val  service: ApiServices
+    private val service: ApiServices
 ) : ViewModel() {
 
+    val analyticsResponseBody: MutableLiveData<Analytics> = MutableLiveData()
+    val loadingVisibility: MutableLiveData<Int> = MutableLiveData()
+    val errorMessage: MutableLiveData<Int> = MutableLiveData()
 
+
+    private lateinit var subscription: Disposable
+
+    fun getAnalytics(scope: String) {
+        subscription = service.analytics(scope)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onRetrieveAnalyticsStart() }
+            .subscribe(
+                { result -> onRetrieveAnalyticsSuccess(result) },
+                { onRetrieveAnalyticsError() }
+            )
+    }
+
+
+    private fun onRetrieveAnalyticsStart() {
+        loadingVisibility.value = View.VISIBLE
+        errorMessage.value = null
+    }
+
+
+    private fun onRetrieveAnalyticsSuccess(analytics: Analytics) {
+        loadingVisibility.value = View.GONE
+
+        analyticsResponseBody.value = analytics
+
+    }
+
+    private fun onRetrieveAnalyticsError() {
+        errorMessage.value = R.string.load_error
+    }
 
 }
